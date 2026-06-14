52692dee8c91dc8857f421c2743619f1f929448f
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-14 17:59:25 +0500

    feat(initiation): AppConfig (Project Initiation & Charter)

---
+++ apps/initiation/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class InitiationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.initiation'
+    verbose_name = 'Project Initiation & Charter'
