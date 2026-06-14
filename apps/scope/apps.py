0dbeb0c5e321ab5e324bc2192c28669580569f6a
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-14 20:58:05 +0500

    feat(scope): AppConfig (Scope & Requirements Management)

---
+++ apps/scope/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ScopeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.scope'
+    verbose_name = 'Scope & Requirements Management'
